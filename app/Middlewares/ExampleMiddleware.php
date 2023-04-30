63121284bcf4a2021bfde9d0a23e5d51ed53ab93
lolilovers <cyntiadewioktavia@gmail.com>
2023-04-30 09:26:22 +0700

    Update ExampleMiddleware.php

--- app/Middlewares/ExampleMiddleware.php
+++ app/Middlewares/ExampleMiddleware.php
@@ -4,13 +4,13 @@ namespace App\Middlewares;
 
 class ExampleMiddleware
 {
-    public static function before()
+    public static function before(): void
     {
-
+        // ...
     }
 
-    public static function after()
+    public static function after(): void
     {
-
+        // ...
     }
 }
